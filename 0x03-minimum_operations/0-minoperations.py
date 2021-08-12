8b2eea1ed37a308cd5be2d641d80cd2adae9496c
Selidex <justindparnell@yahoo.com>
2021-08-12 11:16:58 +0000

    Uploading task 0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Time to do a thing for interviews"""
+
+
+def minOperations(n):
+    """Going to do some weird math here"""
+    m = 0
+    i = 2
+    for i in range(2, n):
+        while(n % i == 0):
+            m += i
+            n = n/i
+    return m
