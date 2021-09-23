ef7c2ced5622bc5adb2aa3a6f200d074ff943621
Selidex <justindparnell@yahoo.com>
2021-09-23 21:03:50 +0000

    Uploading full program

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -14,5 +14,17 @@ def validUTF8(data):
         if num >> 7 == 0:
             i += 1
             continue
+        if num >> 5 == 0b110 and i < x - 1:
+            if data[i + 1] >> 6 == 2:
+                i += 2
+                continue
+        if num >> 4 == 0b1110 and i < x - 2:
+            if data[i + 1] >> 6 == data[i + 2] >> 6 == 2:
+                i += 3
+                continue
+        if num >> 3 == 0b11110 and i < x - 3:
+            if data[i + 1] >> 6 == data[i + 2] >> 6 == data[i + 3] >> 6 == 2:
+                i += 4
+                continue
         return False
     return True
