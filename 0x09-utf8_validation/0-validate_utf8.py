95e344b6d8edbb5fd8303f991bd70ead17d5569a
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-09-23 16:23:29 -0500

    Documenting logic for if statements

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -11,17 +11,21 @@ def validUTF8(data):
     i = 0
     while(i < x):
         num = data[i] & 0xFF
+        """ If the byte starts with 0, its a single character, so continue"""
         if num >> 7 == 0:
             i += 1
             continue
+        """ If the byte starts with 110, it is a two byte character. all bytes that follow the initial byte must start with b10, ie be 2. otherwise return false"""
         if num >> 5 == 0b110 and i < x - 1:
             if data[i + 1] >> 6 == 2:
                 i += 2
                 continue
+        """ 1110 signals a 3 byte character"""
         if num >> 4 == 0b1110 and i < x - 2:
             if data[i + 1] >> 6 == data[i + 2] >> 6 == 2:
                 i += 3
                 continue
+        """ 11110 signals a 4 byte character"""
         if num >> 3 == 0b11110 and i < x - 3:
             if data[i + 1] >> 6 == data[i + 2] >> 6 == data[i + 3] >> 6 == 2:
                 i += 4
