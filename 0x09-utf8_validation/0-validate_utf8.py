59c9cb7cbedade12dcf70e9db666995243a0a25a
Selidex <justindparnell@yahoo.com>
2021-09-23 20:52:19 +0000

    Pep8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,10 +4,11 @@
 Checks to see if a data set is UTF-8
 """
 
+
 def validUTF8(data):
     """ This function does the checking """
     x = len(data)
-    i = 0;
+    i = 0
     while(i < x):
         num = data[i] & 0xFF
         if num >> 7 == 0:
