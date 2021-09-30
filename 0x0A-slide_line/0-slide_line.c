edf28fda65eb679cb2c9be45211666802e5f6838
Selidex <justindparnell@yahoo.com>
2021-09-30 23:18:34 +0000

    Uploading files

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,81 @@
+#include "slide_line.h"
+
+/**
+ * left - slides and merges left
+ * @line: the line being merged
+ * @size: size of the line
+ * Return: 1 for success
+ */
+
+int left(int *line, size_t size)
+{
+	int i = size - 1;
+	int k = 0;
+	int j = size;
+
+	for (; i > 0; i--)
+	{
+		while (line[i] == 0 && i >= 0)
+			i--;
+		k = 1;
+		while (line[i - k] == 0 && (i - k) >= 0)
+			k++;
+		if (line[i] == line[i - k])
+		{
+			line[i] = 0;
+			line[i - k] *= 2;
+		}
+		i -= k - 1;
+	}
+	k = 0;
+	for (i = 0; i < j; i++)
+	{
+		if (line[i] != 0)
+		{
+			line[k] = line[i];
+			if (i != k)
+				line[i] = 0;
+			k++;
+		}
+	}
+	return (1);
+}
+
+/**
+ * right - slides and merges left
+ * @line: the line being merged
+ * @size: size of the line
+ * Return: 1 for success
+ */
+
+int right(int *line, size_t size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (line[i] == 0 && i < size)
+			i++;
+	}
+
+	return (1);
+}
+
+/**
+ * slide_line - mimics the 2048 game on one line
+ * @line: the line being merged
+ * @size: size of the line
+ * @direction: the direction of the merge
+ * Return: 1 on success, 0 on failure
+ */
+
+int slide_line(int *line, size_t size, int direction)
+{
+	if (direction != 1 && direction != -1)
+		return (0);
+
+	if (direction == -1)
+		return (left(line, size));
+	else
+		return (right(line, size));
+}
