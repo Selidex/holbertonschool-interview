fb5b19cac4fe27c2f61678001ca1b2b1dca31731
Selidex <justindparnell@yahoo.com>
2021-11-18 20:55:52 +0000

    Fixing logic, adding recursion for outside walls

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -6,21 +6,22 @@ def rain(walls):
     """ This function does the math"""
     i1 = 0
     i2 = 0
-    v1 = 0
-    v2 = 0
     fall = 0
-    if walls:
-        for x in range(0, len(walls)):
-            if walls[x] > v2:
-                v2 = walls[x]
-                i2 = x
-            if v2 > v1:
-                tmp = v1
-                tmpx = i1
-                v1 = v2
-                i1 = i2
-                v2 = tmp
-                i2 = tmpx
-        return v2 * abs(i1 - i2 + 1)
-    else:
+    if len(walls) < 3:
         return 0
+    for x in range(0, len(walls)):
+        if walls[x] >= walls[i2]:
+            i2 = x
+        if walls[i2] >= walls[i1]:
+            tmp = i2
+            i2 = i1
+            i1 = tmp
+    fall = walls[i2] * (abs(i2 - i1) - 1)
+    if i2 > i1:
+        return (fall + rain(walls[0:i1 + 1]) +
+                rain(walls[i2:]) -
+                sum(walls[i1 + 1: i2]))
+    else:
+        return (fall + rain(walls[0:i2 + 1]) +
+                rain(walls[i1:]) -
+                sum(walls[i2 + 1: i1]))
