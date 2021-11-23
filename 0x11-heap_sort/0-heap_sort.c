271331a40bf181e47b33967c322eed9cd92c8964
Selidex <justindparnell@yahoo.com>
2021-11-23 10:06:58 +0000

    Uploading initial files

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * heap_sort - performs the heap sort on an array
+ * @array: The array being heap sorted
+ * @size: the size of the array
+ * Return: void, no return
+ */
+
+void heap_sort(int *array, size_t size)
+{
+	return;
+}
