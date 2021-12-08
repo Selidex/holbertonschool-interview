0ece31c4ddffcb2b010fe4aa95684366ae1e0f18
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-12-08 16:20:35 -0600

    Initial upload

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This file will be API"""
+import requests
+import sys
+
+
+def recurse(subreddit, hot_list=[]):
+    """Recursion"""
+    if subreddit is None:
+        return None
+    url = "https://www.reddit.com/r/" + str(subreddit) + "/hot/.json"
+    headers = {'User-Agent': 'Sel'}
+    if len(hot_list) < 1:
+        r = requests.get(url, headers=headers, allow_redirects=False)
+        if r.status_code != 200:
+                    return None
+        after = r.json().get('data').get('after')
+        hot_list.append(after)
+    else:
+        url = str(url) + "?after=" + str(hot_list[0]) + "&limit=100"
+        r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code != 200:
+        return None
+    after = r.json().get('data').get('after')
+    hot_list[0] = after
+    rj = r.json().get('data').get('children')
+    for post in rj:
+        child_dict = post['data']
+        hot_list.append(child_dict.get('title'))
+    if after is None:
+        hot_list.pop(0)
+        return hot_list
+    else:
+        return recurse(subreddit, hot_list)
