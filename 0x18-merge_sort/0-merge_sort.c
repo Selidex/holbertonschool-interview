eee5a45584118b5a311753edb64c975846173cd9
Selidex <justindparnell@yahoo.com>
2022-01-20 22:41:56 +0000

    Uploading barebones file

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+
+/**
+ * merge_sort - performs a merge sort on the array
+ * @array: the array being sorted
+ * @size: the size of the array being sorted
+ * Return: void, no return
+ */
+
+void merge_sort(int *array, size_t size)
+{
+	if(array == NULL)
+		return;
+	if(size == 0)
+		return;
+	return;
+}
