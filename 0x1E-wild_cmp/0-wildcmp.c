1a85e0bfa5a4a0a8fabab139c685dc3698d6e386
Selidex <justindparnell@yahoo.com>
2022-03-03 02:01:18 +0000

    Changed return

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -29,10 +29,7 @@ int do_wild(char *s1, char *s2)
 		return (do_wild(s1 + 1, s2 + 1));
 	if (*s2 == '*' && *(s2 + 1) != *(s1 + 1))
 		return (do_wild(s1 + 1, s2));
-	return (do_wild(s1 + 1, s2 + 1)
-		|| do_wild(s1, s2 + 1)
-		|| do_wild(s1 + 1, s2)
-		);
+	return (1);
 }
 
 /**
