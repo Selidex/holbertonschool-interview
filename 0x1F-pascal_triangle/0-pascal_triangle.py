58c92324e43274a5b3b9806f41bb5103e36a11da
Dexter Selina Parnell <justindparnell@yahoo.com>
2022-04-14 13:38:53 -0500

    Initial upload

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This function returns pascal's triangle up to n rows"""
+
+def pascal_triangle(n):
+    """This function return's Pascal's triangle up to n rows"""
+    pascal = []
+    for i in range(n):
+        pascal.append([])
+        pascal[i].append(1)
+        for j in range(1, i):
+            pascal[i].append(pascal[i - 1][j - 1] + pascal[i - 1][j])
+        if(n != 0):
+            pascal[i].append(1)
+    return pascal
